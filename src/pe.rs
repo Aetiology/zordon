227b37728aedaf4495a0b30ec7aff1599fb9800e
Your Name <you@example.com>
2021-04-26 11:09:43 +0100

    Added new function for Pe struct

--- src/pe.rs
+++ src/pe.rs
@@ -1,5 +1,25 @@
 use crate::fmt_err;
 use crate::types::*;
+use crate::{dos_hdr::DosHeader, nt_hdr::*};
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::io::{Read, Write};
 
+pub struct Pe {
+    pub dos_hdr: DosHeader,
+    pub nt_hdr: NtHeader,
+}
+
+impl Pe {
+    pub fn new<R: Read + Seek>(reader: &mut R) -> Result<Self, String> {
+        let dos_hdr = DosHeader::new(reader)?;
+
+        reader
+            .seek(SeekFrom::Start(*dos_hdr.addr_of_new_exe_hdr.val() as u64))
+            .map_err(|e| fmt_err!("Could not seek to nt header start: {}", e))?;
+
+        let nt_hdr = NtHeader::new(reader)?;
+
+        Ok(Self { dos_hdr, nt_hdr })
+    }
+}
