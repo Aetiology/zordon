7b5453e9ae8d45b960cac6aac04b633d0507cc13
Your Name <you@example.com>
2021-05-24 10:19:34 +0100

    Removed pe test data and headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,5 @@
 //#![warn(missing_docs, missing_doc_code_examples)]
 //#![doc(test(no_crate_ionject))]
 #![feature(backtrace)]
-pub mod dos_hdr;
-pub mod imports;
-pub mod nt_hdr;
-pub mod pe;
-pub mod relocs;
-pub mod sec_hdr;
 pub mod types;
-#[macro_use]
-pub mod util;
 pub use mut_view::MutViewNew;
